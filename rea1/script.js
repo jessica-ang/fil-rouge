f9f7e8e9863190c4f9f0adb90f2bb07904f6b02a
jessica-ang <82023240+jessica-ang@users.noreply.github.com>
2021-05-11 15:09:05 +0200

    modif responsive, ajout de fiches.html, icons, contact.html

--- rea1/script.js
+++ rea1/script.js
@@ -1,8 +1,8 @@
 function menuResponsive() {
-    let x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
+    let x = document.getElementById("myMainNav");
+    if (x.className === "mainNav") {
       x.className += " responsive";
     } else {
-      x.className = "topnav";
+      x.className = "mainNav";
     }
   }
