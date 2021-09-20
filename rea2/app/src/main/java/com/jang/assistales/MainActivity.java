050025ea66be4038176bfa824aa0fb581e69bf96
jessica-ang <82023240+jessica-ang@users.noreply.github.com>
2021-09-20 11:55:19 +0200

    ajout design android

--- rea2/app/src/main/java/com/jang/assistales/MainActivity.java
+++ rea2/app/src/main/java/com/jang/assistales/MainActivity.java
@@ -1,14 +1,50 @@
 package com.jang.assistales;
 
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.jang.assistales.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private MainAdapter adapter = new MainAdapter();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.rvMain.setAdapter(adapter);
+    }
+
+    public void onBtHomeClick(View view) {
+    }
+
+    public void onBtProjectClick(View view) {
+    }
+
+    public void onBtUniversClick(View view) {
+    }
+
+    public void onBtCharacterClick(View view) {
+    }
+
+    public void onBtDeitiesClick(View view) {
+    }
+
+    public void onBtPlaceClick(View view) {
+    }
+
+    public void onBtFaunaClick(View view) {
+    }
+
+    public void onBtFloraClick(View view) {
+    }
+
+    public void onBtItemClick(View view) {
     }
 }
